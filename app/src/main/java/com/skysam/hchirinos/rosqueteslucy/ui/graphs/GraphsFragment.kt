ac0b9920f8ebb7c4e7bde65ebd515b821edb2b19
HectorSalo <43446563+HectorSalo@users.noreply.github.com>
2021-08-28 13:47:49 -0400

    v1.1.5 Implementacion MPChart library

--- app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/graphs/GraphsFragment.kt
+++ app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/graphs/GraphsFragment.kt
@@ -1,33 +1,35 @@
 package com.skysam.hchirinos.rosqueteslucy.ui.graphs
 
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.anychart.AnyChart
-import com.anychart.AnyChart.pie
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.chart.common.listener.Event
-import com.anychart.chart.common.listener.ListenersInterface
-import com.anychart.enums.Align
-import com.anychart.enums.LegendLayout
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.skysam.hchirinos.rosqueteslucy.R
+import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Expense
 import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Sale
 import com.skysam.hchirinos.rosqueteslucy.databinding.FragmentGraphsBinding
+import java.util.*
 
 
 class GraphsFragment : Fragment() {
 
     private val viewModel: GraphsViewModel by activityViewModels()
     private var _binding: FragmentGraphsBinding? = null
-    private val sales = mutableListOf<Sale>()
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val sales = mutableListOf<Sale>()
+    private val salesNotPaid = mutableListOf<Sale>()
+    private val salesPaid = mutableListOf<Sale>()
+    private val expenses = mutableListOf<Expense>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,49 +48,90 @@ class GraphsFragment : Fragment() {
     private fun loadViewModel() {
         viewModel.sales.observe(viewLifecycleOwner, {
             if (_binding != null) {
+                salesNotPaid.clear()
+                salesPaid.clear()
                 sales.clear()
                 sales.addAll(it)
+                for (sale in sales) {
+                    if (sale.isPaid) salesPaid.add(sale) else salesNotPaid.add(sale)
+                }
+                loadChart()
+            }
+        })
+        viewModel.expenses.observe(viewLifecycleOwner, {
+            if (_binding != null) {
+                expenses.clear()
+                expenses.addAll(it)
                 loadChart()
             }
         })
     }
 
     private fun loadChart() {
-        val pie = pie()
-       pie.setOnClickListener(object : ListenersInterface.OnClickListener(arrayOf("x", "value")) {
-            override fun onClick(event: Event) {
-                Toast.makeText(
-                    requireContext(),
-                    event.getData().get("x").toString() + ":" + event.getData().get("value"),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        })
+        val pieBalance = binding.pieBalance
+        pieBalance.description = null
+        pieBalance.centerText = "Balance Mensual\n($)"
+        pieBalance.setCenterTextSize(24f)
+        pieBalance.setDrawEntryLabels(false)
+        pieBalance.isRotationEnabled = false
 
-        val data: MutableList<DataEntry> = ArrayList()
-        data.add(ValueDataEntry("Apples", 6371664))
-        data.add(ValueDataEntry("Pears", 789622))
-        data.add(ValueDataEntry("Bananas", 7216301))
-        data.add(ValueDataEntry("Grapes", 1486621))
-        data.add(ValueDataEntry("Oranges", 1200000))
+        var totalSalesPaid = 0.0
+        for (sale in salesPaid) {
+            totalSalesPaid += (sale.quantity * sale.price)
+        }
+        var totalSalesNotPaid = 0.0
+        for (sale in salesNotPaid) {
+            totalSalesNotPaid += (sale.quantity * sale.price)
+        }
+        var totalExpenses = 0.0
+        for (expense in expenses) {
+            totalExpenses += (expense.quantity * expense.price)
+        }
 
-        pie.data(data)
+        val pieEntries = mutableListOf<PieEntry>()
+        pieEntries.add(PieEntry(totalSalesPaid.toFloat(), getString(R.string.text_sales_paid)))
+        pieEntries.add(PieEntry(totalSalesNotPaid.toFloat(), getString(R.string.text_sales_not_paid)))
+        pieEntries.add(PieEntry(totalExpenses.toFloat(), getString(R.string.text_waste)))
+        pieEntries.add(PieEntry(totalExpenses.toFloat(), getString(R.string.title_expenses)))
 
-        pie.title("Fruits imported in 2015 (in kg)")
+        val pieDataSet = PieDataSet(pieEntries, "")
+        pieDataSet.valueTextSize = 18f
+        pieDataSet.setColors(
+            ContextCompat.getColor(requireContext(), R.color.green_light),
+            ContextCompat.getColor(requireContext(), R.color.red_light),
+            ContextCompat.getColor(requireContext(), R.color.yellow),
+            ContextCompat.getColor(requireContext(), R.color.blue)
+        )
+        pieDataSet.formSize = 16f
+        pieDataSet.values[0].data = convertNumber(totalSalesPaid.toString())
+        val pieData = PieData(pieDataSet)
 
-        pie.labels().position("outside")
+        pieBalance.data = pieData
+        pieBalance.legend.textColor = requireContext().resolveColorAttr(android.R.attr.textColorSecondary)
+        pieBalance.invalidate()
 
-        pie.legend().title().enabled(true)
-        pie.legend().title()
-            .text("Retail channels")
-            .padding(0.0, 0.0, 10.0, 0.0)
+        pieBalance.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.GONE
+    }
 
-        pie.legend()
-            .position("center-bottom")
-            .itemsLayout(LegendLayout.HORIZONTAL)
-            .align(Align.CENTER)
+    @ColorInt
+    fun Context.resolveColorAttr(@AttrRes colorAttr: Int): Int {
+        val resolvedAttr = resolveThemeAttr(colorAttr)
+        // resourceId is used if it's a ColorStateList, and data if it's a color reference or a hex color
+        val colorRes = if (resolvedAttr.resourceId != 0) resolvedAttr.resourceId else resolvedAttr.data
+        return ContextCompat.getColor(requireContext(), colorRes)
+    }
 
-        binding.anyChartView.setChart(pie)
+    private fun Context.resolveThemeAttr(@AttrRes attrRes: Int): TypedValue {
+        val typedValue = TypedValue()
+        theme.resolveAttribute(attrRes, typedValue, true)
+        return typedValue
+    }
+
+    private fun convertNumber(number: String): String {
+        val cadena = number.replace(",", "").replace(".", "")
+        val cantidad: Double = cadena.toDouble() / 100
+        return String.format(Locale.GERMANY, "%,.2f", cantidad)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/graphs/GraphsViewModel.kt
+++ app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/graphs/GraphsViewModel.kt
@@ -3,10 +3,13 @@ package com.skysam.hchirinos.rosqueteslucy.ui.graphs
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Expense
 import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Sale
+import com.skysam.hchirinos.rosqueteslucy.database.repositories.ExpensesRepository
 import com.skysam.hchirinos.rosqueteslucy.database.repositories.SalesRepository
 
 class GraphsViewModel : ViewModel() {
 
     val sales: LiveData<MutableList<Sale>> = SalesRepository.getSales().asLiveData()
+    val expenses: LiveData<MutableList<Expense>> = ExpensesRepository.getExpenses().asLiveData()
 }
