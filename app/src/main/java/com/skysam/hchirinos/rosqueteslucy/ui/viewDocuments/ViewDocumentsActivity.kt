1a4b9e7ec47f0a5fb42d91637ad9bde8b5e274e2
HectorSalo <43446563+HectorSalo@users.noreply.github.com>
2021-10-23 12:19:10 -0400

    v5.0.2 Implementacion ultima visita/Liberacion Play Store

--- app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/viewDocuments/ViewDocumentsActivity.kt
+++ app/src/main/java/com/skysam/hchirinos/rosqueteslucy/ui/viewDocuments/ViewDocumentsActivity.kt
@@ -11,13 +11,22 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.skysam.hchirinos.rosqueteslucy.R
+import com.skysam.hchirinos.rosqueteslucy.common.ClassesCommon
 import com.skysam.hchirinos.rosqueteslucy.common.Constants
 import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Costumer
+import com.skysam.hchirinos.rosqueteslucy.common.dataClass.NoteSale
+import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Refund
+import com.skysam.hchirinos.rosqueteslucy.common.dataClass.Sale
 import com.skysam.hchirinos.rosqueteslucy.databinding.ActivityViewDocumentsBinding
+import java.util.*
 
 class ViewDocumentsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityViewDocumentsBinding
     private val viewModel: ViewDocumentsViewModel by viewModels()
+    private val allSales = mutableListOf<Sale>()
+    private val allNotesSale = mutableListOf<NoteSale>()
+    private val allRefunds = mutableListOf<Refund>()
+    private lateinit var costumer: Costumer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +47,7 @@ class ViewDocumentsActivity : AppCompatActivity() {
         val listSorted = mutableListOf<String>()
         val bundle = intent.extras
         if (bundle != null) {
-            val costumer = bundle.get(Constants.ID_COSTUMER) as Costumer
+            costumer = bundle.get(Constants.ID_COSTUMER) as Costumer
             viewModel.addCostumer(costumer)
             supportActionBar?.title = costumer.name
             listSorted.addAll(costumer.locations.sorted())
@@ -55,12 +64,140 @@ class ViewDocumentsActivity : AppCompatActivity() {
                 id: Long
             ) {
                 viewModel.changeLocation(listSorted[position])
+                if (position == 0) findLastDocument()
+                else findLastDocumentByLocation(binding.spinner.selectedItem.toString())
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
         }
+        loadViewModel()
+    }
+
+    private fun loadViewModel() {
+        viewModel.allSales.observe(this, {
+            allSales.clear()
+            for (sale in it) {
+                if (sale.idCostumer == costumer.id) allSales.add(sale)
+            }
+            if (it.isNotEmpty()) {
+                findLastDocument()
+            }
+        })
+        viewModel.allNotesSales.observe(this, {
+            allNotesSale.clear()
+            for (noteSale in it) {
+                if (noteSale.idCostumer == costumer.id) allNotesSale.add(noteSale)
+            }
+            if (it.isNotEmpty()) {
+                findLastDocument()
+            }
+        })
+        viewModel.allRefunds.observe(this, {
+            allRefunds.clear()
+            for (refund in it) {
+                if (refund.idCostumer == costumer.id) allRefunds.add(refund)
+            }
+            if (it.isNotEmpty()) {
+                findLastDocument()
+            }
+        })
+    }
+
+    private fun findLastDocument() {
+        var lastSale: Sale? = null
+        var lastNoteSale: NoteSale? = null
+        var lastRefund: Refund? = null
+
+        if (allSales.isNotEmpty()) lastSale = allSales.first()
+        if (allNotesSale.isNotEmpty()) lastNoteSale = allNotesSale.first()
+        if (allRefunds.isNotEmpty()) lastRefund = allRefunds.first()
+
+        if (lastSale != null && lastNoteSale != null) {
+            if (Date(lastSale.dateDelivery).after(Date(lastNoteSale.dateDelivery)))
+                lastNoteSale = null else lastSale = null
+        }
+        if (lastSale != null && lastRefund != null) {
+            if (Date(lastSale.dateDelivery).after(Date(lastRefund.date)))
+                lastRefund = null else lastSale = null
+        }
+        if (lastNoteSale != null && lastRefund != null) {
+            if (Date(lastNoteSale.dateDelivery).after(Date(lastRefund.date)))
+                lastRefund = null else lastNoteSale = null
+        }
+
+        if (lastSale != null) binding.tvInfo.text = getString(R.string.text_last_visit_all,
+            lastSale.location, ClassesCommon.convertDateToString(Date(lastSale.dateDelivery)),
+            getString(R.string.text_sale_single))
+        if (lastNoteSale != null) binding.tvInfo.text = getString(R.string.text_last_visit_all,
+            lastNoteSale.location, ClassesCommon.convertDateToString(Date(lastNoteSale.dateDelivery)),
+            getString(R.string.text_note_sale_single))
+        if (lastRefund != null) binding.tvInfo.text = getString(R.string.text_last_visit_all,
+            lastRefund.location, ClassesCommon.convertDateToString(Date(lastRefund.date)),
+            getString(R.string.text_refund_single))
+        if (lastSale == null && lastNoteSale == null && lastRefund == null)
+            binding.tvInfo.text= getString(R.string.text_not_last_visit)
+    }
+
+    private fun findLastDocumentByLocation(location: String) {
+        var lastSale: Sale? = null
+        var lastNoteSale: NoteSale? = null
+        var lastRefund: Refund? = null
+
+        val saleFromLocation = mutableListOf<Sale>()
+        val noteSaleFromLocation = mutableListOf<NoteSale>()
+        val refundFromLocation = mutableListOf<Refund>()
+
+        if (allSales.isNotEmpty()) {
+            for (sal in allSales) {
+                if (sal.location == location) saleFromLocation.add(sal)
+            }
+            if (saleFromLocation.isNotEmpty()) {
+                lastSale = saleFromLocation.first()
+            }
+        }
+        if (allNotesSale.isNotEmpty()) {
+            for (noteS in allNotesSale) {
+                if (noteS.location == location) noteSaleFromLocation.add(noteS)
+            }
+            if (noteSaleFromLocation.isNotEmpty()) {
+                lastNoteSale = noteSaleFromLocation.first()
+            }
+        }
+        if (allRefunds.isNotEmpty()) {
+            for (refu in allRefunds) {
+                if (refu.location == location) refundFromLocation.add(refu)
+            }
+            if (refundFromLocation.isNotEmpty()) {
+                lastRefund = refundFromLocation.first()
+            }
+        }
+
+        if (lastSale != null && lastNoteSale != null) {
+            if (Date(lastSale.dateDelivery).after(Date(lastNoteSale.dateDelivery)))
+                lastNoteSale = null else lastSale = null
+        }
+        if (lastSale != null && lastRefund != null) {
+            if (Date(lastSale.dateDelivery).after(Date(lastRefund.date)))
+                lastRefund = null else lastSale = null
+        }
+        if (lastNoteSale != null && lastRefund != null) {
+            if (Date(lastNoteSale.dateDelivery).after(Date(lastRefund.date)))
+                lastRefund = null else lastNoteSale = null
+        }
+
+        if (lastSale != null) binding.tvInfo.text = getString(R.string.text_last_visit_location,
+            ClassesCommon.convertDateToString(Date(lastSale.dateDelivery)),
+            getString(R.string.text_sale_single))
+        if (lastNoteSale != null) binding.tvInfo.text = getString(R.string.text_last_visit_location,
+            ClassesCommon.convertDateToString(Date(lastNoteSale.dateDelivery)),
+            getString(R.string.text_note_sale_single))
+        if (lastRefund != null) binding.tvInfo.text = getString(R.string.text_last_visit_location,
+            ClassesCommon.convertDateToString(Date(lastRefund.date)),
+            getString(R.string.text_refund_single))
+        if (lastSale == null && lastNoteSale == null && lastRefund == null)
+            binding.tvInfo.text= getString(R.string.text_not_last_visit)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
